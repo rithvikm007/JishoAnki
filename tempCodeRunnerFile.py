150337319aa2e0826f9e1bba4eea72977f49b1dd
M Rithvik Reddy <rithvikreddym2006@gmail.com>
2025-08-04 22:45:54 +0530

    delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-raw_decks = fetch_deck_names()
-# decks = ["Select deck"] + raw_decks
-# selected_deck = ctk.StringVar(value="Select deck")
-# deck_dropdown = ctk.CTkOptionMenu(app, values=decks, variable=selected_deck)
-# deck_dropdown.pack(pady=10)
